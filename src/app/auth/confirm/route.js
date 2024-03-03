27de6f41a9adb3ae49b0b35cbf727a5ab296cbe3
Ifeoluwa Odubela <i.odubela@gmail.com>
2024-03-04 01:52:09 +0400

    fix wrong import

--- src/app/auth/confirm/route.js
+++ src/app/auth/confirm/route.js
@@ -1,7 +1,7 @@
 import {  } from '@supabase/supabase-js'
 import {  NextResponse } from 'next/server'
 
-import { createClient } from '@/utils/supabase/server'
+import { createClient } from '@/lib/supabase/server'
 
 export async function GET(request) {
   const { searchParams } = new URL(request.url)
