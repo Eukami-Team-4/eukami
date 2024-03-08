d7d06fac1f29379937268e3a2671d4c32c911f6d
Usman <iqbalu4@aston.ac.uk>
2024-03-08 11:32:07 +0000

    add profile page

---
+++ src/app/myaccount/profile/page.js
@@ -0,0 +1,11 @@
+const ProfilePage = () => {
+    return (
+        <div>
+            <h1>
+            This is rhe profile page
+            </h1>
+        </div>
+    );
+}
+
+export default ProfilePage;
