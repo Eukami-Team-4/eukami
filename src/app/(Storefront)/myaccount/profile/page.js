9c1306408ff07d46d73d79fae5b43606f3834557
Ifeoluwa Odubela <i.odubela@gmail.com>
2024-03-14 16:49:25 +0000

    add pencil to the top input
    Co-Authored-By: Usman91919 <151534236+Usman91919@users.noreply.github.com>

--- src/app/(Storefront)/myaccount/profile/page.js
+++ src/app/(Storefront)/myaccount/profile/page.js
@@ -1,6 +1,9 @@
+import { Input } from "@/components/ui/input";
+import { Pencil } from "lucide-react";
+
 const ProfilePage = () => {
     return (
-        <div>
+        <div className="flex flex-col">
             <h1 className="flex items-center justify-center w-full h-20 mt-20 mb-5 text-4xl font-bold uppercase">
             PROFILE
             </h1>
@@ -10,9 +13,9 @@ const ProfilePage = () => {
                 </div>
             </div>
 
-            <div className ="flex items-center justify-center mb-10">
-                <input className="w-2/5 h-10 text-xs bo"
-                type="text" id="name" name="name" placeholder="       Name"></input>
+            <div className ="relative flex items-center justify-center w-2/5 mx-auto mb-10">
+                <Input className=" h-10 block w-full rounded-none py-1.5 pr-10 bg-white border-white text-xs"
+                type="text" id="name" name="name" placeholder="Name"></Input><Pencil size={16} className="absolute pointer-events-none right-4"/>
             </div>
 
             <div className ="flex items-center justify-center mb-10">
