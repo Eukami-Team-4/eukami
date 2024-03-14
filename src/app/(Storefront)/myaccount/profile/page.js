320d43493428b6235d84fc3ef9914c2090e28f60
Usman <iqbalu4@aston.ac.uk>
2024-03-14 16:04:09 +0000

    The profile page I have produced so far.

---
+++ src/app/(Storefront)/myaccount/profile/page.js
@@ -0,0 +1,39 @@
+const ProfilePage = () => {
+    return (
+        <div>
+            <h1 className="flex items-center justify-center w-full h-20 mt-20 mb-5 text-4xl font-bold uppercase">
+            PROFILE
+            </h1>
+            
+            <div className ="flex items-center justify-center mb-10">
+                <div className="rounded-full w-28 h-28 w bg-slate-300">
+                </div>
+            </div>
+
+            <div className ="flex items-center justify-center mb-10">
+                <input className="w-2/5 h-10 text-xs bo"
+                type="text" id="name" name="name" placeholder="       Name"></input>
+            </div>
+
+            <div className ="flex items-center justify-center mb-10">
+                <input className="w-2/5 h-10 text-xs bo"
+                type="text" id="name" name="name" placeholder="       Email"></input>
+            </div>
+
+
+            <div className ="flex items-center justify-center mb-10">
+                <input className="w-2/5 h-10 text-xs text-gray-500"
+                type="text" id="name" name="name" placeholder="       Phone Number"></input>
+            </div>
+
+            <div className ="flex items-center justify-center mb-10">
+                <button>Save</button>
+            </div>
+            
+
+            
+        </div>
+    );
+}
+
+export default ProfilePage;
