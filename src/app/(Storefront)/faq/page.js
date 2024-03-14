db7b85f3e6cec3df821afca7058cd4a2722af3a5
Muhtasib1 <muhtasib123@hotmail.co.uk>
2024-03-14 19:48:34 +0000

    Update page.js

--- src/app/(Storefront)/faq/page.js
+++ src/app/(Storefront)/faq/page.js
@@ -12,22 +12,23 @@ const FaqPage = () => {
       <h1 className="text-red-500">FAQ</h1>
       <Accordion type="single" collapsible className="w-full">
         <AccordionItem value="item-1">
-          <AccordionTrigger>titlwelefwefwefwfe</AccordionTrigger>
+          <AccordionTrigger>Can I return my order in-store?</AccordionTrigger>
           <AccordionContent>
-            Yes. It adheres to the WAI-ARIA design pattern.
+            Yes, online orders can be refunded in-store. You will have 28 days to exchange or return your order within any Eukami store. After the 28 days refunds are no longer possible and your item cannot be exchanged.
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-2">
-          <AccordionTrigger>Is it styled?</AccordionTrigger>
+          <AccordionTrigger>Has my order been returned?</AccordionTrigger>
           <AccordionContent>
-            Yes. It comes with default styles that matches the other
-            components&apos; aesthetic.
+            Refunds and exchanges will be processed within 10 working days of the parcel arriving at our returns in our warehouse.
+            Please note it typically takes 7 days for the courier to deliver your return parcel. We do not offer refunds on items damaged without the purchased warranty.
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-3">
-          <AccordionTrigger>Is it animated?</AccordionTrigger>
+          <AccordionTrigger>Email Newsletter</AccordionTrigger>
           <AccordionContent>
-            Yes. It's animated by default, but you can disable it if you prefer.
+          To sign up to our email newsletter detailing our new produts and price cuts, enter your email on the sign-up page and submit
+            To unsubscribe, click on the link at the bottom of the newsletter.
           </AccordionContent>
         </AccordionItem>
       </Accordion>
