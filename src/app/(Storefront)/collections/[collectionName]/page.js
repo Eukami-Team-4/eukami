bbed4afd2066f852f6976d38745c5d85481dbd88
enzoriise <davidoduekun@gmail.com>
2024-03-15 10:47:40 +0000

    some layout fixes

--- src/app/(Storefront)/collections/[collectionName]/page.js
+++ src/app/(Storefront)/collections/[collectionName]/page.js
@@ -5,73 +5,78 @@ import { cn } from "@/lib/utils";
 import Image from "next/image";
 
 const CollectionPage = ({ params }) => {
+    return (
+        <main className="pb-16 space-y-16">
+            <section className="flex items-center justify-center w-full h-40 text-4xl font-medium uppercase bg-indigoDye text-seasalt">
+                {params.collectionName}
+            </section>
 
-  return (
-    <main className="pb-16 space-y-16">
-      <section className="flex items-center justify-center w-full h-40 text-4xl font-medium uppercase bg-indigoDye text-seasalt">
-        {params.collectionName}
-      </section>
-
-      {Products.map((product, index) => (
-        <ProductCard
-          key={product.name}
-          collectionName={params.collectionName}
-          product={product}
-          reversed={index % 2 === 0}
-        />
-      ))}
-      <FeaturedCollection />
-      <UniqueSellingPoint />
-    </main>
-  );
+            {Products.map((product, index) => (
+                <ProductCard
+                    key={product.name}
+                    collectionName={params.collectionName}
+                    product={product}
+                    reversed={index % 2 === 0}
+                />
+            ))}
+            <FeaturedCollection />
+            <UniqueSellingPoint />
+        </main>
+    );
 };
 
 const Products = [
-  {
-    name: "Product 1",
-    image: "/images/hp1.png",
-    description:
-      "Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.",
-    isFeatured: true,
-  },
-  {
-    name: "Product 2",
-    image: "/images/hp1.png",
-    description:
-      "Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.",
-  },
+    {
+        name: "Product 1",
+        image: "/images/hp1.png",
+        description:
+            "Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.",
+        isFeatured: true,
+    },
+    {
+        name: "Product 2",
+        image: "/images/hp1.png",
+        description:
+            "Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.",
+    },
 ];
 
 const ProductCard = ({ product, reversed, collectionName }) => {
-  return (
-    <section
-      className={cn(
-        "container relative flex flex-col items-center justify-between h-full gap-12 py-8 rounded-lg lg:flex-row text-seasalt",
-        reversed && "lg:flex-row-reverse"
-      )}
-    >
-      <div className="flex flex-col justify-center h-full gap-5 text-center lg:w-1/2 lg:text-left">
-        {product.isFeatured && (
-          <h3 className="uppercase text-brightOrange">Featured Product</h3>
-        )}
-        <h1 className="text-5xl uppercase text-indigoDye">{collectionName}{" "}{product.name}</h1>
-        <p className="text-onyx">{product.description}</p>
-        <div>
-          <StorefrontButton href="/products/1">See Product</StorefrontButton>
-        </div>
-      </div>
-      <div className="relative flex items-center justify-center w-full p-8 aspect-square lg:w-1/2">
-        <div>
-          <Image
-            src={product.image}
-            fill
-            className="object-contain rounded-lg lg:h-72"
-            alt={product.name}
-          ></Image>
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <section
+            className={cn(
+                "container relative flex flex-col items-center justify-between h-full gap-12 py-8 rounded-lg lg:flex-row text-seasalt",
+                reversed && "lg:flex-row-reverse"
+            )}
+        >
+            <div className="flex flex-col justify-center h-full gap-5 text-center lg:w-1/2 lg:text-left">
+                {product.isFeatured && (
+                    <h3 className="uppercase text-brightOrange">
+                        Featured Product
+                    </h3>
+                )}
+                <h1 className="text-5xl uppercase text-indigoDye">
+                    {collectionName} {product.name}
+                </h1>
+                <p className="text-onyx">{product.description}</p>
+                <div>
+                    <StorefrontButton href="/products/1">
+                        See Product
+                    </StorefrontButton>
+                </div>
+            </div>
+            <div className="relative flex items-center justify-center w-full p-8 aspect-square lg:w-1/2">
+                <div>
+                    <Image
+                        src={product.image}
+                        fill
+                        className="object-contain rounded-lg lg:h-72"
+                        alt={product.name}
+                    ></Image>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default CollectionPage;
