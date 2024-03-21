9590096d9158d1664a98275f6208164c2060086c
Usman <iqbalu4@aston.ac.uk>
2024-03-21 15:59:38 +0000

    added layout

--- src/app/(Storefront)/saved-cards/page.js
+++ src/app/(Storefront)/saved-cards/page.js
@@ -1,7 +1,22 @@
+import {Button} from "@/components/ui/button.jsx"
+import React from "react";
+import card from "public\Images\cart-image.png"
 const savedCards = () => {
     return (
         <div>
-            <p1>Saved cards</p1>
+            
+
+            <main>
+                <div className="flex items-center justify-between w-auto h-40 text-4xl font-medium uppercase">
+                    <h1 className=" ml-36">Manage Addresses</h1>
+                    <Button className="mr-36 bg-onyx">Add card+</Button>
+                </div>
+
+                <div className="grid w-auto h-auto grid-cols-2 px-6 py-8 mb-10 bg-antiFlashWhite pb rounded-xl ml-36 mr-36">
+                    <div className=""> <img src={card} alt="card image" /></div>
+                </div>
+
+            </main>
         </div>
     );
 }
