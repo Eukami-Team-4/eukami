579b4b680b2f90cf2643809bbd2de138b8f873fe
Usman <iqbalu4@aston.ac.uk>
2024-03-21 21:12:28 +0000

    button changed

--- src/app/(Storefront)/saved-cards/page.js
+++ src/app/(Storefront)/saved-cards/page.js
@@ -9,7 +9,7 @@ const savedCards = () => {
             <main>
                 <div className="flex items-center justify-between w-auto h-40 text-4xl font-medium uppercase">
                     <h1 className=" ml-36">Saved cards</h1>
-                    <Button className="mr-36 bg-onyx">Add card+</Button>
+                    <Button className="mr-36 bg-onyx hover:bg-onyx hover:opacity-85">Add card+</Button>
                 </div>
 
                 <div className="grid w-auto h-auto grid-cols-2 gap-6 px-6 py-8 mb-10 bg-antiFlashWhite pb rounded-xl ml-36 mr-36">
