0ac133a0cdf4af348b91299268473c8425934c05
Muhtasib1 <muhtasib123@hotmail.co.uk>
2024-03-22 10:41:41 +0000

    Update page.js

--- src/app/(Storefront)/aboutus/page.js
+++ src/app/(Storefront)/aboutus/page.js
@@ -1,6 +1,8 @@
 const AboutUs= () => {
     return (
+        
         <div className="bg-[#0C233B] text-white text-center">
+            <img src="public/Images/locationSymbol.png" alt="Man with Headphones" className="w-full h-auto"/>
             Welcome to EUKAMI, where our passion for flawless audio meets innovative design to provide you with a unique sound experience.
         </div>
     );
