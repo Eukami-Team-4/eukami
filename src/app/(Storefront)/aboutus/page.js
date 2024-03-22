a459cddd1390b0da60b1ade0b6e485b7e8556979
Muhtasib1 <muhtasib123@hotmail.co.uk>
2024-03-22 10:58:38 +0000

    added image to about us

--- src/app/(Storefront)/aboutus/page.js
+++ src/app/(Storefront)/aboutus/page.js
@@ -1,10 +1,17 @@
+import Image from "next/image"
 const AboutUs= () => {
     return (
-        
-        <div className="bg-[#0C233B] text-white text-center">
-            <img src="public/Images/locationSymbol.png" alt="Man with Headphones" className="w-full h-auto"/>
+        <div className="bg-[#0C233B] text-white text-center ">
+            <Image
+                src="hp1.png" 
+                alt="Man with Headphones"
+                className="w-full h-auto"
+            ></Image>
             Welcome to EUKAMI, where our passion for flawless audio meets innovative design to provide you with a unique sound experience.
         </div>
+
+
+
     );
 }
 
