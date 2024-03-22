41c46ae7026635c5a2ab201e18566502ea3f1afc
Muhtasib1 <muhtasib123@hotmail.co.uk>
2024-03-22 11:10:11 +0000

    fixed picture issue
    fixed error picture caused, assigned width and height

--- src/app/(Storefront)/aboutus/page.js
+++ src/app/(Storefront)/aboutus/page.js
@@ -1,14 +1,18 @@
 import Image from "next/image"
 const AboutUs= () => {
     return (
-        <div className="bg-[#0C233B] text-white text-center ">
-            <Image
-                src="hp1.png" 
-                alt="Man with Headphones"
-                className="w-full h-auto"
-            ></Image>
-            Welcome to EUKAMI, where our passion for flawless audio meets innovative design to provide you with a unique sound experience.
+        <div className="bg-[#0C233B] text-white text-center"> 
+        <div className="relative w-full h-[600px]">
+        <Image
+            src="/images/hp1.png" 
+            fill
+            alt="Man with Headphones"
+            className="object-cover"
+        ></Image>
         </div>
+        Welcome to EUKAMI, where our passion for flawless audio meets innovative design to provide you with a unique sound experience.
+        </div>
+
 
 
 
