d17e85fe1b24ee33f05bc4b695991860dcb134c9
Muhtasib1 <muhtasib123@hotmail.co.uk>
2024-03-22 10:20:06 +0000

    Update page.js

--- src/app/(Storefront)/aboutus/page.js
+++ src/app/(Storefront)/aboutus/page.js
@@ -1,1 +1,9 @@
+const AboutUs= () => {
+    return (
+        <div className="bg-[#0C233B] text-white text-center">
+            Welcome to EUKAMI, where our passion for flawless audio meets innovative design to provide you with a unique sound experience.
+        </div>
+    );
+}
 
+export default AboutUs;
