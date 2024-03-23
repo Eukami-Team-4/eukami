1317b53531f180b1dce093e3a548fe141867b2b4
Usman <iqbalu4@aston.ac.uk>
2024-03-23 16:15:21 +0000

    Sorted bug (duplicate file) and added more styling

--- src/app/(Storefront)/MyOrdersInDetail/page.js
+++ src/app/(Storefront)/MyOrdersInDetail/page.js
@@ -9,16 +9,16 @@ const MyOrdersInDetail = () => {
             </div>
             <div className="w-auto h-40 pl-2 border-b border-slate-300">
                 <p className="font-semibold">Order Status</p>
-                <p className ="mt-4 ml-2">shipped</p>
-                <p className ="mt-4 ml-2">delivered</p>
-                <p className ="mt-4 ml-2">cancelled</p>
+                <p className ="mt-4 ml-2 cursor-pointer hover:underline">shipped</p>
+                <p className ="mt-4 ml-2 cursor-pointer hover:underline">delivered</p>
+                <p className ="mt-4 ml-2 cursor-pointer hover:underline">cancelled</p>
             </div>
             <div className="pl-2">
             <p className="font-semibold">Order Time</p>
-                <p className ="mt-4 ml-2">Last 30 days</p>
-                <p className ="mt-4 ml-2">Last 3 months</p>
-                <p className ="mt-4 ml-2">2023</p>
-                <p className ="mt-4 ml-2">2022</p>
+                <p className ="mt-4 ml-2 cursor-pointer hover:underline">Last 30 days</p>
+                <p className ="mt-4 ml-2 cursor-pointer hover:underline">Last 3 months</p>
+                <p className ="mt-4 ml-2 cursor-pointer hover:underline">2023</p>
+                <p className ="mt-4 ml-2 cursor-pointer hover:underline">2022</p>
             </div>
           </div>
             
